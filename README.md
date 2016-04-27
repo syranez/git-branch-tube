d636894abd8d7cff3f22f1843cd32ce906a7d033
syranez <syranez@minaga-church.de>
2016-04-27 22:16:12 +0200

    git-branch-tube creates a git branch with commits, branches and merges.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # git-branch-tube
 A bash script to generate a git repo with lots of commits, branches and merges.
+
+![Screenshot](syranez.github.com/git-branch-tube.png)
+
+## Installation
+
+    npm install
+
+## Usage
+
+   ./index.bash && npm run show
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ index.bash
@@ -0,0 +1,76 @@
+#! /usr/bin/env bash
+#
+
+# directory in which a repo is created
+REPOSITORY_DIR="repository";
+
+# maximum number of actions
+MAX_ACTIONS=500;
+
+# maximum commits per action
+MAX_COMMITS_PER_ACTION=10;
+
+# intern: current branch name
+current_branch=1;
+
+# creates the repo directory and initialises git
+function init {
+
+    mkdir "${REPOSITORY_DIR}";
+    cd "${REPOSITORY_DIR}";
+
+    git init
+}
+
+# makes a merge or branch
+function decideBranchOrMerge {
+
+    number=$RANDOM;
+    let "number >>= 14"
+    if [ "$number" -eq 1 ]; then
+
+        if [ "$current_branch" -eq "2" ]; then
+            git checkout master;
+            git merge "$current_branch" --no-ff -m "Merge: $(npm run -s lorem-ipsum 1 sentence)";
+            current_branch=1;
+        else 
+            git checkout $((current_branch - 1));
+            git merge "$current_branch" --no-ff -m "Merge: $(npm run -s lorem-ipsum 1 sentence)";
+            current_branch=$((current_branch - 1));
+        fi
+    else
+        current_branch=$((current_branch + 1));
+        git branch "$current_branch";
+        git checkout "$current_branch";
+    fi
+}
+
+# creates $1 commit objects
+function createCommits {
+
+    for ((i=1; i <= "${1}"; i++)); do
+        echo "${i}" >> "file";
+        git add "file";
+        git commit -m "$(npm run -s lorem-ipsum 1 sentence)" >/dev/null;
+    done;
+}
+
+# creates a git repo with branches and merges
+function createRepo {
+
+    init;
+
+    for ((i=1; i <= "${1}"; i++)); do
+
+        # how many commits
+        count=$RANDOM
+        let "count %= ""${2}";
+
+        echo "Creating ${count} commits."
+        createCommits "${count}";
+
+        decideBranchOrMerge
+    done;
+}
+
+createRepo "${MAX_ACTIONS}" "${MAX_COMMITS_PER_ACTION}";
